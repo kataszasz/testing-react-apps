15a3614056808d331741786f31ed7b3dc00f2e2a
Kata Szasz <Kata@invevo.com>
2021-01-19 06:29:13 +0000

    solve testinng-react-app 04 - extra4

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,14 +5,14 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
-import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
 
   render(<Login onSubmit={handleSubmit} />)
 
-  const {username, password} = buildLoginForm({password: '123'})
+  const {username, password} = buildLoginForm()
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
 
@@ -22,14 +22,12 @@ test('submitting the form calls onSubmit with username and password', () => {
   expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
 
-function buildLoginForm(overrides) {
-  return ({
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...overrides
-  })
-}
-
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password())
+  }
+})
 /*
 eslint
   no-unused-vars: "off",
