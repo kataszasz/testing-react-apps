11a1b1080846dd675c97be30c650cd974fdd8167
Kata Szasz <Kata@invevo.com>
2021-01-19 07:25:02 +0000

    solve testinng-react-app 05 - extra1

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -7,6 +7,7 @@ import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
 import {rest} from 'msw' 
 import {setupServer} from 'msw/node'
+import {handlers} from 'test/server-handlers'
 import Login from '../../components/login-submission'
 
 const buildLoginForm = build({
@@ -16,18 +17,7 @@ const buildLoginForm = build({
   },
 })
 
-const server = setupServer(
-  rest.post('https://auth-provider.example.com/api/login',
-             async (req, res, ctx) => {
-               if (!req.body.password) {
-                 return res(ctx.status(400), ctx.json({message: 'password is required'}))
-               } 
-               if (!req.body.username) {
-                 return res(ctx.status(400), ctx.json({message: 'username is required'}))
-               }
-               return res(ctx.json({username: req.body.username}))
-             })
-)
+const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
